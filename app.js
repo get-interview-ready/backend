b9b957545a196a8146ce390c694aedb1effeebfb
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-07-30 18:20:00 +0530

    fix: cors

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
+// const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+// app.use(cors());
 
 const user = require("./routes/user");
 const dreamCompany = require("./routes/dreamCompany");
