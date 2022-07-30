465bed00b4acad3e29ca03d48b85260883f8e753
Kedar Basutkar <krb1202@gmail.com>
2022-07-30 15:23:03 +0530

    feat: behavioral questions routes

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ app.use(cookieParser());
 
 const user = require("./routes/user");
 const dreamCompany = require("./routes/dreamCompany");
+const behavioralQuestion = require("./routes/behavioralQuestion");
 
 app.use("/api/v1/", user);
 app.use("/api/v1/dreamCompanies", dreamCompany);
+app.use("/api/v1/behavioralQuestions", behavioralQuestion);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello world!" });
