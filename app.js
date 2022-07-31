3b575f4cc526b27d8c3833105a2b827e0442f773
Kedar Basutkar <krb1202@gmail.com>
2022-07-31 15:01:52 +0530

    feat: flash card routes

--- app.js
+++ app.js
@@ -13,17 +13,18 @@ const user = require("./routes/user");
 const dreamCompany = require("./routes/dreamCompany");
 const behavioralQuestion = require("./routes/behavioralQuestion");
 const project = require("./routes/project");
-const technicalQuestion = require('./routes/technicalQuestion')
+const technicalQuestion = require("./routes/technicalQuestion");
+const flashCard = require("./routes/flashCard");
 
 app.use("/api/v1/", user);
 app.use("/api/v1/dreamCompanies", dreamCompany);
 app.use("/api/v1/behavioralQuestions", behavioralQuestion);
 app.use("/api/v1/projects", project);
 app.use("/api/v1/technicalQuestions", technicalQuestion);
+app.use("/api/v1/flashCards", flashCard);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello world!" });
 });
 
 exports.app = app;
-
